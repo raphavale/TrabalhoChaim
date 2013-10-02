fe7fc4c88961b7b0c7eda57a42c91ba0f21777e5
Raphael Vale <raphaavale@gmail.com>
2013-10-02 19:56:23 -0300

    Comentando o código...

--- src/br/com/dev/Main.java
+++ src/br/com/dev/Main.java
@@ -6,10 +6,14 @@ public class Main {
 	
 	//Função Exemplo TestCase
 	
+	
+	//Soma só inteiros positivos
 	public int somaPositivo(int a, int b){
 		return (Math.abs(a) + Math.abs(b));
 	}
 	
+	
+	//Soma só inteiros negativos
 	public int somaNegativo(int a, int b){
 		return ((Math.abs(a))*-1) + (Math.abs(b)*-1); 
 	}
