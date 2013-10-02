7312820aedc0b180b30f2113efe587fa56211a30
Raphael Vale <raphaavale@gmail.com>
2013-10-02 19:46:15 -0300

    Adicionado o testcase, e dois métodos fictícios em Main.java.

--- src/br/com/dev/Main.java
+++ src/br/com/dev/Main.java
@@ -3,5 +3,14 @@ package br.com.dev;
 public class Main {
 	
 	//Aguardando definição de trabalho.
-
+	
+	//Função Exemplo TestCase
+	
+	public int somaPositivo(int a, int b){
+		return (Math.abs(a) + Math.abs(b));
+	}
+	
+	public int somaNegativo(int a, int b){
+		return ((Math.abs(a))*-1) + (Math.abs(b)*-1); 
+	}
 }
